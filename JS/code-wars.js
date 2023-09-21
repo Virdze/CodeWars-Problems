8ca2086954cefa1cfb7e5696f647e1d3a72d2ba4
João André <joao.andre.freitas@hotmail.com>
2023-09-21 12:42:27 +0100

    Added js files

--- JS/code-wars.js
+++ JS/code-wars.js
@@ -163,3 +163,51 @@ const grow_v2 = x => {
     }
     return res;
 };
+
+/**
+ * Create a function that takes 2 integers in form of a string as an input, and outputs the sum (also as a string):
+
+Example: (Input1, Input2 -->Output)
+
+"4",  "5" --> "9"
+"34", "5" --> "39"
+"", "" --> "0"
+"2", "" --> "2"
+"-5", "3" --> "-2"
+Notes:
+
+If either input is an empty string, consider it as zero.
+
+Inputs and the expected output will never exceed the signed 32-bit integer limit (2^31 - 1)
+*/
+
+const sumStr =(a,b) => {
+    return (a === "" && b === "") ? '0' :
+           (b === "") ? (parseInt(a)).toString() :
+           (a === "") ? (parseInt(b)).toString() : (parseInt(a) + parseInt(b)).toString();
+}
+
+//Other solutions
+
+function sumStr(a,b) {
+    return String(Number(a)+Number(b));
+}
+
+
+function sumStr(a,b) {
+    /**
+     * (+a + +b):
+
+            The + symbol before each variable a and b is used to explicitly convert the string values to numbers. This process is known as type coercion or type casting.
+            By using +a, you're converting the a string to a numeric value.
+            Similarly, +b converts the b string to a numeric value.
+            Then, it adds these two numeric values together.
+            + '':
+
+            After performing the addition, the result is implicitly converted back to a string by adding an empty string ('') to it. This is done because the function is expected to return a string.
+            return statement:
+
+            The result of the addition, which is now a string, is returned from the function.
+     */
+    return (+a+ +b)+''  
+}
